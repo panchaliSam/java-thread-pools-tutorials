c258da87be5545f4d7e8c8b3b397f4ffa175fb8d
panchaliSam <panchalisam20030117@gmail.com>
2025-07-07 16:23:11 +0530

    first commit

---
+++ src/main/java/org/example/javathreadpoolstutorials/JavaThreadPoolsTutorialsApplication.java
@@ -0,0 +1,13 @@
+package org.example.javathreadpoolstutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThreadPoolsTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaThreadPoolsTutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/javathreadpoolstutorials/JavaThreadPoolsTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javathreadpoolstutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaThreadPoolsTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
